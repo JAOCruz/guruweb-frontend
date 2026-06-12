8457e5d2d464cb813778075b903aae11fa04d048
Jay <jay@amazingprosthetic.com>
2026-06-12 00:32:32 +0000

    fix(typewriter): zoom out model, remove background glow

--- src/components/TypewriterSection.tsx
+++ src/components/TypewriterSection.tsx
@@ -18,8 +18,8 @@ function TypewriterModel() {
   return (
     <group
       ref={groupRef}
-      scale={1.4}
-      position={[0, -0.4, 0]}
+      scale={1.0}
+      position={[0, -0.8, 0]}
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)}
     >
@@ -30,9 +30,9 @@ function TypewriterModel() {
 
 function TypewriterCanvas() {
   return (
-    <div className="relative h-[24rem] w-full md:h-[32rem] lg:h-[40rem]">
+    <div className="relative h-[28rem] w-full md:h-[36rem] lg:h-[44rem]">
       <Canvas
-        camera={{ position: [0, 1.5, 8], fov: 55 }}
+        camera={{ position: [0, 1.0, 12], fov: 60 }}
         style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
       >
         <ambientLight intensity={1.2} />
@@ -50,9 +50,6 @@ function TypewriterCanvas() {
 const TypewriterSection: React.FC = () => {
   return (
     <section className="relative overflow-hidden bg-[#020617] py-16 md:py-24">
-      {/* Subtle radial glow behind the model */}
-      <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_center,_rgba(30,64,175,0.25)_0%,_transparent_70%)]" />
-
       <div className="relative z-10 mx-auto max-w-5xl px-4 sm:px-6">
         <motion.div
           initial={{ opacity: 0, y: 40 }}
